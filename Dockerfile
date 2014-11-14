3ea45110d579c52a9c86a5bbd40d9de6985c6dda
Zack YL Shih <zackyl.shih@moxa.com>
2014-11-14 17:33:21 +0800

    Add .dockerignore, Dockerfile, server.sh and fix typo

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM dockerfile/nodejs
+
+MAINTAINER Zack YL Shih <zackyl.shih@moxa.com>
+
+ADD . /data
+WORKDIR /data
+
+RUN npm install
+
+EXPOSE 8080
+
+CMD npm start
--- server.sh
+++ server.sh
@@ -2,5 +2,5 @@
 
 node_modules/mosca/bin/mosca -v --http-static . \
     --http-port 8080 \
-    --parent-port $BROKER_PORT_1883_TCP_ADDR \
-    --parent-host $BROKER_PORT_1883_TCP_PORT
+    --parent-port $BROKER_PORT_1883_TCP_PORT \
+    --parent-host $BROKER_PORT_1883_TCP_ADDR
