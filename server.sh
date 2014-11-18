510cea2ba3bf863714cb0eca862a93ea353c8533
Zack YL Shih <zackyl.shih@moxa.com>
2014-11-18 13:52:40 +0800

    Add bunyan for mosca logging pretty

--- server.sh
+++ server.sh
@@ -3,4 +3,5 @@
 node_modules/mosca/bin/mosca -v --http-static . \
     --http-port 8080 \
     --parent-port $BROKER_PORT_1883_TCP_PORT \
-    --parent-host $BROKER_PORT_1883_TCP_ADDR
+    --parent-host $BROKER_PORT_1883_TCP_ADDR \
+    | bunyan
